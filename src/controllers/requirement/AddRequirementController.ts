388eaaeafe59945ca27263d55b8cbde166ed81b9
pedro.darde <pedro.darde@retta.com.br>
2022-03-17 17:53:06 -0300

    ...

--- src/controllers/requirement/AddRequirementController.ts
+++ src/controllers/requirement/AddRequirementController.ts
@@ -1,17 +1,22 @@
 import { getCustomRepository } from "typeorm";
-import { ok } from "../../helpers/http-helper";
+import { ok, serverError } from "../../helpers/http-helper";
 import { Controller } from "../../protocols/Controller";
 import { HttpRequest, HttpResponse } from "../../protocols/http";
 import { RequirementPostgresRepository } from "../../repositories/RequirementPostgresRepository";
 
 export class AddRequirementController implements Controller {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { requirement } = httpRequest.body;
-    const repo = this.makeRepository();
-    const requirementParsed = repo.create(requirement);
-    const requirementInserted = await repo.add(requirementParsed);
+    try {
+      const { requirement } = httpRequest.body;
+      const repo = this.makeRepository();
+      const requirementParsed = repo.create(requirement);
+      const requirementInserted = await repo.add(requirementParsed);
 
-    return ok(requirementInserted);
+      return ok(requirementInserted);
+    } catch (err) {
+      console.error(err);
+      return serverError(err);
+    }
   }
 
   private makeRepository() {
--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -4,7 +4,14 @@ import setupMiddlaweres from "./middlewares";
 import setupRoutes from "./routes";
 const app = express();
 
+app.get("/", (req, res) => {
+    res.end("<h1> Project requirement API </h1>")
+})
+
+app.get("/api", (req, res) => {
+    res.end("<h1> Project requirement API </h1>")
+})
+
 setupMiddlaweres(app);
 setupRoutes(app);
-app.use("/uploads/products", express.static(path.join(__dirname, "..", '..', "uploads/products")));
 export default app;
